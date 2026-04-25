22c106dcc97f658232a3e6de169b34f904c88927
Alejandra <ale.or2405@gmail.com>
2026-04-25 03:07:06 +0200

    Tengo que arreglar que cadenas de supermercados se conecte a la bd y que aparezcan todos los filtros en tiendas, mañana lo hago que tengo mucho sueño

--- backend/src/main/java/es/grupo8/backend/dao/CampaignStoreRepository.java
+++ backend/src/main/java/es/grupo8/backend/dao/CampaignStoreRepository.java
@@ -1,9 +1,14 @@
 package es.grupo8.backend.dao;
-
+ 
+import java.util.List;
+ 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+ 
 import es.grupo8.backend.entity.CampaignStore;
 import es.grupo8.backend.entity.CampaignStoreId;
-
+ 
 public interface CampaignStoreRepository extends JpaRepository<CampaignStore, CampaignStoreId> {
+ 
+    /** Devuelve todas las CampaignStore asociadas a una tienda concreta */
+    List<CampaignStore> findByIdStore_Id(Integer storeId);
 }
