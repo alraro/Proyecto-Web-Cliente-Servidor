95afd48dbbdd5c1c757b67e23e7181f24ce0235a
fernan92005 <fernan925@uma.es>
2026-04-25 13:42:27 +0200

    Add methods to CaptainRepository and CoordinatorRepository for campaign-related queries

--- backend/src/main/java/es/grupo8/backend/dao/CaptainRepository.java
+++ backend/src/main/java/es/grupo8/backend/dao/CaptainRepository.java
@@ -1,9 +1,21 @@
 package es.grupo8.backend.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 import es.grupo8.backend.entity.Captain;
 import es.grupo8.backend.entity.CaptainId;
 
 public interface CaptainRepository extends JpaRepository<Captain, CaptainId> {
+
+	List<Captain> findByIdIdCampaign(Integer idCampaign);
+
+	boolean existsByIdIdUserAndIdIdCampaign(Integer idUser, Integer idCampaign);
+
+	@Transactional
+	@Modifying
+	void deleteByIdIdUserAndIdIdCampaign(Integer idUser, Integer idCampaign);
 }
--- backend/src/main/java/es/grupo8/backend/dao/CoordinatorRepository.java
+++ backend/src/main/java/es/grupo8/backend/dao/CoordinatorRepository.java
@@ -1,9 +1,21 @@
 package es.grupo8.backend.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 import es.grupo8.backend.entity.Coordinator;
 import es.grupo8.backend.entity.CoordinatorId;
 
 public interface CoordinatorRepository extends JpaRepository<Coordinator, CoordinatorId> {
+
+	List<Coordinator> findByIdIdCampaign(Integer idCampaign);
+
+	boolean existsByIdIdUserAndIdIdCampaign(Integer idUser, Integer idCampaign);
+
+	@Transactional
+	@Modifying
+	void deleteByIdIdUserAndIdIdCampaign(Integer idUser, Integer idCampaign);
 }
