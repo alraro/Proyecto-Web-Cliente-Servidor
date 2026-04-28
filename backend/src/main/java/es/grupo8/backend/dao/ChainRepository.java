4ca2adc198ad15801875afa08e2f20d6e15ba685
Alejandra <ale.or2405@gmail.com>
2026-04-28 11:56:32 +0200

    Ahora no se baja la tienda o cadena al editarla, tambien responsable tienda esta accesible

--- backend/src/main/java/es/grupo8/backend/dao/ChainRepository.java
+++ backend/src/main/java/es/grupo8/backend/dao/ChainRepository.java
@@ -1,5 +1,6 @@
 package es.grupo8.backend.dao;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import es.grupo8.backend.entity.ChainEntity;
 
 public interface ChainRepository extends JpaRepository<ChainEntity, Integer> {
+
+    List<ChainEntity> findAllByOrderByIdChainAsc();
 
     Optional<ChainEntity> findByCode(String code);
 
