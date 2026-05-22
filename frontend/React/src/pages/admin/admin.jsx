491768480de976e8b75b562035a2207f2ae77c40
Chewi9 <hugohg@uma.es>
2026-05-22 20:48:17 +0200

    Creada bases de react para comenzar a trabajar

---
+++ frontend/React/src/pages/admin/admin.jsx
@@ -0,0 +1,9 @@
+function Admin() {
+
+    return (
+        <div>
+            <h1>Página de administrador</h1>
+        </div>
+    );
+}
+export default Admin;
---
+++ frontend/React/src/pages/coordinator/coordinator.jsx
@@ -0,0 +1,9 @@
+function Coordinator() {
+return (
+    <div>
+        <h1>Página de coordinador</h1>
+    </div>
+);
+}
+
+export default Coordinator;
