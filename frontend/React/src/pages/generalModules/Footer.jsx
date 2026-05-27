66b35ffdeccb0c3191a3f9aaf61559374820a813
alraro <alfonsoramosrojas@gmail.com>
2026-05-27 10:29:44 +0200

    Creados algunos modulos comunes base

---
+++ frontend/React/src/pages/generalModules/Footer.jsx
@@ -0,0 +1,12 @@
+
+function Footer() {
+    return (
+        <>
+        <div className="footer">
+            <p>Este es el pie de página de los módulos generales.</p>
+        </div>
+        </>
+    );
+}
+
+export default Footer;
---
+++ frontend/React/src/pages/generalModules/Header.jsx
@@ -0,0 +1,12 @@
+
+function Header() {
+    return (
+        <>
+        <div className="header">
+            <h1>Welcome to the General Modules Page</h1>
+        </div>
+        </>
+    );
+}
+
+export default Header;
