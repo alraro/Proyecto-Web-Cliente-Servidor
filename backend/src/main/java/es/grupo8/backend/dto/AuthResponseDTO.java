3b374cbdcf1d514fa3b1944b0ada2f2b8a51cfcf
Chewi9 <hugohg@uma.es>
2026-06-01 17:38:54 +0200

    Minimas cosas

--- backend/src/main/java/es/grupo8/backend/dto/AuthResponseDTO.java
+++ backend/src/main/java/es/grupo8/backend/dto/AuthResponseDTO.java
@@ -11,11 +11,9 @@ public class AuthResponseDTO {
     private String phone;
     private String address;
     private String postalCode;
-
     private String role;
     private String redirectUrl;
     private Integer storeId;
-
     private String message;
     private String token;
     private String tokenType;
